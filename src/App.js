1bf1e93d619fddb51bd1a1f85153c530523ffff5
loughrao <loughrao@oregonstate.edu>
2018-12-15 16:51:21 -0800

    More refactoring

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import NavBar from "./components/NavBar";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Homepage from "./components/webpages/Homepage";
+import Home from "./components/pages/Home";
+import AboutUs from "./components/pages/AboutUs"
+import BaseTheme from "./components/themes/BaseTheme"
 
-// Material ui theme
-const bla = createMuiTheme({
-	palette: {
-		primary: {
-			main: '#d73f09'
-			//light: "",
-			//dark: "",
-			//contrastText: ""
-		},
-		secondary: {
-			main: '#212121'
-		}
-		//error: { main: '' }
-	}
-})
-
-class App extends Component {
-	render() {
-    return (
-		<div>
-			<MuiThemeProvider theme={bla}>
+const App = () => (
+	<Router>
+		<MuiThemeProvider theme={BaseTheme} >
+			<header>
 				<NavBar />
-				<Homepage />
-			</MuiThemeProvider>
-		</div>
-    );
-  }
-}
+			</header>
+			<main>
+				<Switch>
+					<Route path="/home" component={Home} />
+					<Route path="/aboutus" component={AboutUs} />
+				</Switch>
+			</main>
+		</MuiThemeProvider>
+	</Router>
+)
 
 export default App;
