321b1480a6435576cdc032488df88168159d0a2a
loughrao <loughrao@oregonstate.edu>
2019-01-09 22:06:56 -0800

    Updates

---
+++ precache-manifest.f677ca6c6f15989dc9d4e1f89dad60ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d9f77131923cbf5447d",
+    "url": "/static/css/main.c49a5dfd.chunk.css"
+  },
+  {
+    "revision": "3d9f77131923cbf5447d",
+    "url": "/static/js/main.3d9f7713.chunk.js"
+  },
+  {
+    "revision": "62df989575f7ba93b679",
+    "url": "/static/js/1.62df9895.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea727de11f005e618ad170a79437372a",
+    "url": "/static/media/Oregon_State_University-2019-PDR-Report.ea727de1.pdf"
+  },
+  {
+    "revision": "83dd66b5832614dad44737a7b59a3b61",
+    "url": "/static/media/OSU_USLI_2019_CDR.83dd66b5.pdf"
+  },
+  {
+    "revision": "6c9b89cd605a28a71ead1537039e354c",
+    "url": "/index.html"
+  }
+];
